96f586bdd04689b43bca4263fbe3e6008be3698c
André Prates <engandreprates@gmail.com>
2024-01-30 23:30:16 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,6 +29,12 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	//código para limpar a tela
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 
 	// o Scanner será instanciado no prog principal e aqui será recebido ele como
 	// argumento
